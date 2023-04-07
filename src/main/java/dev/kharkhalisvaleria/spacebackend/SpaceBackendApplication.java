2eb00c3ba6d1ef7fa908685d6355bb18145da29e
Avalor <104781846+HarhalisValeria@users.noreply.github.com>
2023-04-07 13:39:21 +0300

    DB connection

---
+++ src/main/java/dev/kharkhalisvaleria/spacebackend/SpaceBackendApplication.java
@@ -0,0 +1,14 @@
+package dev.kharkhalisvaleria.spacebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class SpaceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kharkhalisvaleria/spacebackend/SpaceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kharkhalisvaleria.spacebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
